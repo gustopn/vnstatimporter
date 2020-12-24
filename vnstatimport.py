f7b12b6d4f008b2ca3b9ec481bac67b8272d66ab
Lars Schotte <gustik@gustik.eu>
2020-12-24 04:17:57 +0100

    create a config file

--- vnstatimport.py
+++ vnstatimport.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 from subprocess import Popen, PIPE
-from json import loads
+from json import loads, dumps
 from datetime import date
 import socket
+import os
 
 def get_statistics():
   statisticslist = []
@@ -30,5 +31,26 @@ def get_statistics():
       } )
   return statisticslist
 
+def get_configuration():
+  configuration = None
+  homedir = os.getenv("HOME")
+  if os.path.isdir(homedir):
+    conffile = str.join("/", [homedir, ".vnstatdbimport.conf"])
+    if os.path.isfile(conffile):
+      conf = open(conffile, "r")
+      configuration = loads(conf.read())
+      conf.close()
+    elif not os.path.exists(conffile):
+      conf = open(conffile, "w")
+      configuration = {}
+      configuration["dbname"] = input("What is your database name? ")
+      configuration["user"] = input("What is your user name? ")
+      conf.write(dumps(configuration))
+      conf.close()
+    else:
+      print("something gone wrong, skipping")
+  return configuration
+
 if __name__ == "__main__":
   statisticslist = get_statistics()
+  configuration = get_configuration()
